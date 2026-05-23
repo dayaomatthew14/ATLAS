8abd2891747d9403d8da3e6548bf4905d51596b2
Matthew  Dayao <dayaomatthew14@gmail.com>
2026-05-24 03:50:11 +0800

    chore: replace existing database file with new atlas_v3.db instance

--- backend/check_faculty.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('atlas_v3.db')
-cursor = conn.cursor()
-cursor.execute("SELECT COUNT(*) FROM faculty")
-print("Faculty count:", cursor.fetchone()[0])
-conn.close()
--- backend/fix_sem_casing.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('atlas_v3.db')
-c = conn.cursor()
-c.execute("UPDATE semesters SET term='3rd semester' WHERE term='3rd Semester'")
-conn.commit()
-print('Rows updated:', c.rowcount)
-conn.close()
